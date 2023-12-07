242b3da29801958934d1e25af25e45defde4fee8
ilianaDhbw <ili.schotter.23@lehre.mosbach.dhbw.de>
2023-12-07 21:03:52 +0100

    neu

---
+++ codes/probe.py
@@ -0,0 +1,9 @@
+start_price = 4.80
+km = int(input("Bitte Kilometer eingeben: "))
+if km > 5:
+    costs = 1.8
+else: 
+    costs=2.1
+
+total=start_price + costs * km
+print("Deine Kosten sind:",total )
