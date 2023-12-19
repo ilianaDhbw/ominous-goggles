6b24940330f89b7ca8549c5e5337d33a1a42166b
ilianaDhbw <ili.schotter.23@lehre.mosbach.dhbw.de>
2023-12-19 23:20:38 +0100

    Haus vom Nikolaus

---
+++ codes/test.py
@@ -0,0 +1,26 @@
+import time
+import turtle as t
+from random import randint as rnd
+
+colors = ["light blue","pink","purple","yellow"]
+t.width (6) 
+
+def tl(winkel,mass):
+    t.color(colors[rnd(0,len(colors)-1)])
+    t.left(winkel)
+    t.forward(mass)
+            
+def tr(winkel,mass):
+    t.color(colors[rnd(0,len(colors)-1)])
+    t.right(winkel)
+    t.forward(mass)
+        
+# Programm ablauf
+tl(180,50)
+tr(135,70)
+tl(135,50)
+tr(135,35)
+tr(90,35)
+tr(45,50)
+tr(135,70)
+tl(135,50)
