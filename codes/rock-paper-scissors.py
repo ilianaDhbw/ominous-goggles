ba061195acea4cf4a060e6d6cebaed8fe21cb3a0
ilianaDhbw <ili.schotter.23@lehre.mosbach.dhbw.de>
2024-01-11 18:33:49 +0100

    farben

--- codes/rock-paper-scissors.py
+++ codes/rock-paper-scissors.py
@@ -6,19 +6,19 @@ computer_action = random.choice(possible_actions)
 print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
 
 if user_action == computer_action:
-    print(f"Both players selected {user_action}. It's a tie!")
+    print(f"Both players selected {user_action}." + "\033[33m" +  "It's a tie!")
 elif user_action == "rock":
     if computer_action == "scissors":
-        print("Rock smashes scissors! You win!")
+        print("Scissors cuts paper!"+"\033[35m" + "You win!")
     else:
-        print("Paper covers rock! You lose.")
+        print("Paper covers rock!"+ "\033[36m" + "You lose.")
 elif user_action == "paper":
     if computer_action == "rock":
-        print("Paper covers rock! You win!")
+        print("Scissors cuts paper!"+"\033[35m" + "You win!")
     else:
-        print("Scissors cuts paper! You lose.")
+        print("Paper covers rock!"+ "\033[36m" + "You lose.")
 elif user_action == "scissors":
     if computer_action == "paper":
-        print("Scissors cuts paper! You win!")
+        print("Scissors cuts paper!"+"\033[35m" + "You win!")
     else:
-        print("Rock smashes scissors! You lose.")
+        print("Paper covers rock!"+ "\033[36m" + "You lose.")
